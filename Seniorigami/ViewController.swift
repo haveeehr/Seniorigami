bc601a210eaef4ec14352b3990bcc180ec9cabe9
Javier Fransiscus <53168317+Javierr9@users.noreply.github.com>
2021-04-06 12:53:52 +0700

    Initial Commit

---
+++ Seniorigami/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Seniorigami
+//
+//  Created by Javier Fransiscus on 06/04/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
