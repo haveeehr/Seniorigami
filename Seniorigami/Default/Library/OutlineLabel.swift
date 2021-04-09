946fbc89d0a717a5afb02f3472edeebc99a51da4
yahyayyash <yahyayyashaa@gmail.com>
2021-04-09 09:54:20 +0700

    Play Now page update except cell header.

---
+++ Seniorigami/Default/Library/OutlineLabel.swift
@@ -0,0 +1,29 @@
+//
+//  OutlineLabel.swift
+//  Seniorigami
+//
+//  Created by Yahya Ayyash on 09/04/21.
+//
+
+import UIKit
+
+extension UILabel{
+    
+    func makeOutLine(oulineColor: UIColor, foregroundColor: UIColor){
+        let strokeTextAttributes = [
+            NSAttributedString.Key.strokeColor : oulineColor,
+            NSAttributedString.Key.foregroundColor : foregroundColor,
+            NSAttributedString.Key.strokeWidth : -1.0,
+            NSAttributedString.Key.font : self.font!
+            ] as [NSAttributedString.Key : Any]
+        self.attributedText = NSMutableAttributedString(string: self.text ?? "", attributes: strokeTextAttributes)
+    }
+    
+    func underline() {
+        if let textString = self.text {
+            let attributedString = NSMutableAttributedString(string: textString)
+            attributedString.addAttribute(NSAttributedString.Key.underlineStyle, value: NSUnderlineStyle.single.rawValue, range: NSRange(location: 0, length: attributedString.length))
+            attributedText = attributedString
+        }
+    }
+}
