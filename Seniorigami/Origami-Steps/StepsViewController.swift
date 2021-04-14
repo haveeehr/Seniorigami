1e0d0cf4fc31d046ec14196748261b592b3e3936
ghirlandaioKuo <ghirlandaio27@gmail.com>
2021-04-14 21:27:26 +0700

    Final Commit - No Commentary

--- Seniorigami/Origami-Steps/StepsViewController.swift
+++ Seniorigami/Origami-Steps/StepsViewController.swift
@@ -2,7 +2,7 @@
 //  StepViewController.swift
 //  Seniorigami
 //
-//  Created by Javier Fransiscus on 06/04/21.
+//  Created by Ghirlandaio on 13/04/21.
 //
 
 import UIKit
@@ -16,8 +16,6 @@ class StepsViewController: UIViewController {
     var selected : Origami? = nil
     let stepCellID = "StepCell"
     var dataLog = Database.shared.getLog()
-    
-    // var test = Database.shared.getOrigamiList()[0]
     
     
     override func viewDidLoad() {
@@ -44,7 +42,6 @@ class StepsViewController: UIViewController {
         }
     }
     
-    //4.0 Mengirimkan state finished kepada congratulation view
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         
         Database.shared.setOrigamiFinished(origami: selected!)
@@ -54,50 +51,30 @@ class StepsViewController: UIViewController {
         let destination = segue.destination as? CongratulationsViewController
         
         destination?.selectedOrigami = selected
-        
-        
-        /* test button
-         var finishedState = Database.shared.getOrigamiList()[0]
-         
-         if finishedState.finished == true{
-         print("true")
-         }else{
-         print("false")
-         }
-         
-         print("test button")
-         */
-        
-        
     }
     
 }
 
 extension StepsViewController:UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout{
     
-    //notes 1.1: nambahin features ke class yang udah
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         return CGSize(width: collectionView.frame.width-40, height: collectionView.frame.height-40)
     }
     
-    //notes 1.2: ngitung size collection view brp cell.
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return (selected?.instructions!.count)!
     }
     
     
-    // notes 1.4: kaya naruh cell didalam collection view sesuai index (jadi jumlah si cell), as! itu nge recast
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         var cell = stepsCollection.dequeueReusableCell(withReuseIdentifier: stepCellID, for: indexPath) as! StepCell
         let color = UIColor.init(named: (selected?.mode?.color)!)
-        //notes 1.5: nampung UIImage (cuman gara2 string di database, jadi harus di cast sebagai image disini)
         var imgs: [UIImage] = []
-        //notes 1.6: ... itu maksudnya "sampai dengan"
         for index in 0...(selected?.instructions![indexPath.row].images.count)!-1{
-            imgs.append(UIImage(named: (selected?.instructions![indexPath.row].images[index]!)!)!) //notes 1.7: dia masukin string name si image (dr DB) kedalam si UIimage jadi nanti image2 yang ada di asset tinggal dipanggil aja
+            imgs.append(UIImage(named: (selected?.instructions![indexPath.row].images[index]!)!)!)
         }
-        cell.origamiStepImg.image = UIImage.animatedImage(with: imgs, duration: 3) //notes 1.8: automatis image sequencing pake array
-        cell.origamiStepCount.text = "\(indexPath.row+1) of \((selected?.steps)!)" // notes 1.9: String interpolation
+        cell.origamiStepImg.image = UIImage.animatedImage(with: imgs, duration: 3)
+        cell.origamiStepCount.text = "\(indexPath.row+1) of \((selected?.steps)!)"
         cell.origamiStepCount.sizeToFit()
         cell.origamiStepDesc.text = selected?.instructions![indexPath.row].desc
         cell.origamiStepCount.layer.backgroundColor = color?.cgColor
@@ -105,9 +82,9 @@ extension StepsViewController:UICollectionViewDelegate, UICollectionViewDataSour
         
         return cell
         
-    }//notes 1.3: ngeload cellnya si collection view, cell diisi apa?
+    }
     
-    //notes 2.0: ngatur centering
+    
     func scrollViewWillEndDragging(_ scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset tartgetContentOffset: UnsafeMutablePointer<CGPoint>)
     {
         let layout = self.stepsCollection?.collectionViewLayout as!  UICollectionViewFlowLayout
@@ -120,13 +97,11 @@ extension StepsViewController:UICollectionViewDelegate, UICollectionViewDataSour
         
         let roundedIndex = round(index)
         offset = CGPoint(x: roundedIndex * cellWidthIncludingSpacing - scrollView.contentInset.left ,y: 0)
-        //print("roundedIndex: \(roundedIndex) -- ofset: \(offset)")
         tartgetContentOffset.pointee = offset
         
-        //2.1 menyimpan progress dari step terakhir
         Database.shared.setLog(origami: selected!, steps: Int(roundedIndex))
         
-        //2.2 mengatur availability dari Finish button
+        
         if(Int(roundedIndex) == ((selected?.steps!)! - 1)){
             self.navigationItem.rightBarButtonItem?.isEnabled = true
         }else{
@@ -138,7 +113,6 @@ extension StepsViewController:UICollectionViewDelegate, UICollectionViewDataSour
 }
 
 
-// 3.0 Melakukan forcelock pada velocity sehingga slide dapat berjalan dengan lancar
 class CollectionViewFlowLayout: UICollectionViewFlowLayout {
     override func targetContentOffset(forProposedContentOffset proposedContentOffset: CGPoint, withScrollingVelocity velocity: CGPoint) -> CGPoint
     {
